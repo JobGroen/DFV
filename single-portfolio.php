df9ad9c8380d6458d3cc929d1291b5b10563ab2b
Colin Harzing <colinharzing@hotmail.com>
2019-04-24 14:17:48 +0200

    Single portfolio page aangepast + css
    Single portfolio page aangepast + css

--- single-portfolio.php
+++ single-portfolio.php
@@ -1,21 +1,24 @@
 <?php get_header();?>
+<?php $featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full'); ?>
+
+<div id="header" style="background-image: url(<?php echo esc_url($featured_img_url); ?>)">
+	<div class="header" >
+		<h1 class="standard-h1"><?php the_title();?></h1>
+	</div>
+</div>
 
 <div class="container pt-5 pb-5">
 
-    <h1><?php the_title();?></h1>
-    test test test
+	<p class="standard-h4 portfolio-quote">
+		 <?php the_field('quote'); ?>
+	</p>
+		<?php if (have_posts()) : while(have_posts()) : the_post();?>
 
-    <?php if(has_post_thumbnail()):?>
-    
-        <img src="<?php the_post_thumbnail_url('largest');?>" class="img-fluid">
+			<div class="justify-content-center portfolio-single-text">
+        		<?php the_content(); ?>
+    	</div>
 
-    <?php endif;?>
-
-    <?php if (have_posts()) : while(have_posts()) : the_post();?>
-
-        <?php the_content();?>
-
-    <?php endwhile; endif;?>
+		<?php endwhile; endif;?>
 
 </div>
 
