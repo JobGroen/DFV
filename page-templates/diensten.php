a4023db870ec938b858fb952f50e5ae84d5d805b
Colin Harzing <colinharzing@hotmail.com>
2019-05-09 11:37:29 +0200

    Theme version 0.2.3
    Version 0.2.3 DFV Theme by:
    - Colin Harzing
    
    This contains:
    - Diensten pagina aangepast

--- page-templates/diensten.php
+++ page-templates/diensten.php
@@ -1,23 +1,245 @@
 <?php /* Template Name: Diensten */ ?>
-
+<?php $featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full'); ?>
 <?php get_header();?>
 
-<div class="container pt-5 pb-5">
-
-    <h1><?php the_title();?></h1>
-
-    <?php if(has_post_thumbnail()):?>
-    
-        <img src="<?php the_post_thumbnail_url('largest');?>" class="img-fluid">
-
-    <?php endif;?>
-
-    <?php if (have_posts()) : while(have_posts()) : the_post();?>
-
-        <?php the_content();?>
-
-    <?php endwhile; endif;?>
-
+<div id="header" style="background-image: url(<?php echo esc_url($featured_img_url); ?>)">
+	<div class="header" >
+		<h1 class="standard-h1"><?php the_title();?></h1>
+	</div>
 </div>
 
+   <?php if(get_field('dienst_aanzetten') == "Ja"): ?>
+		<div id="diensten-single">
+			<div class="row">
+				<div class="col-md-5">
+					<div class="container">
+					</div>
+				</div>
+
+				<div class="col-md-7">
+					<div class="container">
+						<h2><?php the_field('dienst_titel');?></h2>
+						<div class="diensten-single-box">
+							<img src="<?php the_field('dienst_afbeelding'); ?>" alt="dienst">
+							<p><?php the_field('dienst_beschrijving'); ?></p>
+
+							<p class="diensten-prijs">€ <?php the_field('prijs'); ?>,-</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php endif; ?>
+
+	<?php if(get_field('dienst_aanzetten_2') == "Ja"): ?>
+		<div id="diensten-single">
+			<div class="row">
+
+				<div class="col-md-7">
+					<div class="container">
+						<h2><?php the_field('dienst_titel_2');?></h2>
+						<div class="diensten-single-box diensten-afbeelding-right">
+							<p><?php the_field('dienst_beschrijving_2'); ?></p>
+
+							<p class="diensten-prijs">€ <?php the_field('prijs_2'); ?>,-</p>
+							<img src="<?php the_field('dienst_afbeelding_2'); ?>" alt="dienst">
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-5">
+					<div class="container">
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php endif; ?>
+
+	<?php if(get_field('dienst_aanzetten_3') == "Ja"): ?>
+			<div id="diensten-single">
+				<div class="row">
+					<div class="col-md-5">
+						<div class="container">
+						</div>
+					</div>
+
+					<div class="col-md-7">
+						<div class="container">
+							<h2><?php the_field('dienst_titel_3');?></h2>
+							<div class="diensten-single-box">
+								<img src="<?php the_field('dienst_afbeelding_3'); ?>" alt="dienst">
+								<p><?php the_field('dienst_beschrijving_3'); ?></p>
+
+								<p class="diensten-prijs">€ <?php the_field('prijs_3'); ?>,-</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+	<?php endif; ?>
+
+	<?php if(get_field('dienst_aanzetten_4') == "Ja"): ?>
+		<div id="diensten-single">
+			<div class="row">
+
+				<div class="col-md-7">
+					<div class="container">
+						<h2><?php the_field('dienst_titel_4');?></h2>
+						<div class="diensten-single-box diensten-afbeelding-right">
+							<p><?php the_field('dienst_beschrijving_4'); ?></p>
+
+							<p class="diensten-prijs">€ <?php the_field('prijs_4'); ?>,-</p>
+							<img src="<?php the_field('dienst_afbeelding_4'); ?>" alt="dienst">
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-5">
+					<div class="container">
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php endif; ?>
+
+	<?php if(get_field('dienst_aanzetten_5') == "Ja"): ?>
+		<div id="diensten-single">
+			<div class="row">
+				<div class="col-md-5">
+					<div class="container">
+					</div>
+				</div>
+
+				<div class="col-md-7">
+					<div class="container">
+						<h2><?php the_field('dienst_titel_5');?></h2>
+						<div class="diensten-single-box">
+							<img src="<?php the_field('dienst_afbeelding_5'); ?>" alt="dienst">
+							<p><?php the_field('dienst_beschrijving_5'); ?></p>
+
+							<p class="diensten-prijs">€ <?php the_field('prijs_5'); ?>,-</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php endif; ?>
+
+	<?php if(get_field('dienst_aanzetten_6') == "Ja"): ?>
+		<div id="diensten-single">
+			<div class="row">
+
+				<div class="col-md-7">
+					<div class="container">
+						<h2><?php the_field('dienst_titel_6');?></h2>
+						<div class="diensten-single-box diensten-afbeelding-right">
+							<p><?php the_field('dienst_beschrijving_6'); ?></p>
+
+							<p class="diensten-prijs">€ <?php the_field('prijs_6'); ?>,-</p>
+							<img src="<?php the_field('dienst_afbeelding_6'); ?>" alt="dienst">
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-5">
+					<div class="container">
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php endif; ?>
+
+	<?php if(get_field('dienst_aanzetten_7') == "Ja"): ?>
+		<div id="diensten-single">
+			<div class="row">
+				<div class="col-md-5">
+					<div class="container">
+					</div>
+				</div>
+
+				<div class="col-md-7">
+					<div class="container">
+						<h2><?php the_field('dienst_titel_7');?></h2>
+						<div class="diensten-single-box">
+							<img src="<?php the_field('dienst_afbeelding_7'); ?>" alt="dienst">
+							<p><?php the_field('dienst_beschrijving_7'); ?></p>
+
+							<p class="diensten-prijs">€ <?php the_field('prijs_7'); ?>,-</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php endif; ?>
+
+	<?php if(get_field('dienst_aanzetten_8') == "Ja"): ?>
+		<div id="diensten-single">
+			<div class="row">
+
+				<div class="col-md-7">
+					<div class="container">
+						<h2><?php the_field('dienst_titel_8');?></h2>
+						<div class="diensten-single-box diensten-afbeelding-right">
+							<p><?php the_field('dienst_beschrijving_8'); ?></p>
+
+							<p class="diensten-prijs">€ <?php the_field('prijs_8'); ?>,-</p>
+							<img src="<?php the_field('dienst_afbeelding_8'); ?>" alt="dienst">
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-5">
+					<div class="container">
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php endif; ?>
+
+	<?php if(get_field('dienst_aanzetten_9') == "Ja"): ?>
+		<div id="diensten-single">
+			<div class="row">
+				<div class="col-md-5">
+					<div class="container">
+					</div>
+				</div>
+
+				<div class="col-md-7">
+					<div class="container">
+						<h2><?php the_field('dienst_titel_9');?></h2>
+						<div class="diensten-single-box">
+							<img src="<?php the_field('dienst_afbeelding_9'); ?>" alt="dienst">
+							<p><?php the_field('dienst_beschrijving_9'); ?></p>
+
+							<p class="diensten-prijs">€ <?php the_field('prijs_9'); ?>,-</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php endif; ?>
+
+	<?php if(get_field('dienst_aanzetten_10') == "Ja"): ?>
+		<div id="diensten-single">
+			<div class="row">
+
+				<div class="col-md-7">
+					<div class="container">
+						<h2><?php the_field('dienst_titel_10');?></h2>
+						<div class="diensten-single-box diensten-afbeelding-right">
+							<p><?php the_field('dienst_beschrijving_10'); ?></p>
+
+							<p class="diensten-prijs">€ <?php the_field('prijs_10'); ?>,-</p>
+							<img src="<?php the_field('dienst_afbeelding_10'); ?>" alt="dienst">
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-5">
+					<div class="container">
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php endif; ?>
 <?php get_footer();?>
