728673ac7f21937aeddc2ab4457dcb62daaab5f9
Jort Broekhof <jortdus@gmail.com>
2019-05-13 11:59:25 +0200

    Theme version 0.2.10
    Version 0.2.10 DFV Theme by:
    - Jort Broekhof
    
    This contains:
    location template part
    event plugin set up

---
+++ template-parts/location.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template part for displaying contact information and location
+ */
+
+?>
+
+<div id="location">
+    <div class="row">
+        <div class="col-md-7">
+            <div class="col-md-5">
+                <h3>Contactgegevens:</h3>
+                <p>Voorbeeldlaan 57<br>
+                    1742 RL<br>
+                    Amsterdam</p><br>
+                <p><i class="far fa-envelope"></i> info@purefaceart.nl</p>
+                <p><i class="fas fa-phone phone-upside"></i> 0612345678</p>
+                <p><i class="fab fa-instagram"></i> /purefaceart</p>
+                <p><i class="fab fa-facebook-square"></i> /purefaceart</p>
+            </div>
+        </div>
+    </div>        
+</div>
