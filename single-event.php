d4b8cfdd5a001aec4341965b4399724f5c0a8937
Jort Broekhof <jortdus@gmail.com>
2019-05-14 13:43:23 +0200

    Theme version 0.2.12
    Version 0.2.10 DFV Theme by:
    - Jort Broekhof
    
    This contains:
    Styling
    Contact page
    Event page

--- single-event.php
+++ single-event.php
@@ -6,21 +6,8 @@
 
 <div id="header" style="background-image: url(<?php echo esc_url($featured_img_url); ?>)">
 	<div class="header" >
-		<h1 class="standard-h1"><?php the_title();?></h1>
+		<div class="event-info"><?php the_content(); ?></div>
 	</div>
 </div>
 
-<div class="container pt-5 pb-5">
-
-	<p class="standard-h4 portfolio-quote">
-		 <?php the_field('quote'); ?>
-	</p>
-		<?php if (have_posts()) : while(have_posts()) : the_post();?>
-
-			<div class="justify-content-center portfolio-single-text">
-        		<?php the_content(); ?>
-			</div>
-
-        <?php endwhile; endif;?>
-</div>
 <?php get_footer();?>
--- template-parts/location.php
+++ template-parts/location.php
@@ -6,18 +6,25 @@
 ?>
 
 <div id="location">
-    <div class="row">
-        <div class="col-md-7">
-            <div class="col-md-5">
-                <h3>Contactgegevens:</h3>
-                <p>Voorbeeldlaan 57<br>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h3 id="location-title">Locatie</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <p>Verzetlaan 57<br>
                     1742 RL<br>
-                    Amsterdam</p><br>
+                    Schagen</p><br>
                 <p><i class="far fa-envelope"></i> info@purefaceart.nl</p>
                 <p><i class="fas fa-phone phone-upside"></i> 0612345678</p>
                 <p><i class="fab fa-instagram"></i> /purefaceart</p>
                 <p><i class="fab fa-facebook-square"></i> /purefaceart</p>
             </div>
-        </div>
-    </div>        
+            <div class="col-md-8">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2413.412274420692!2d4.796934116139701!3d52.77887357986396!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47cf4e27375bf591%3A0x34ce63a49e06995c!2sVerzetlaan+57%2C+1742+RL+Schagen!5e0!3m2!1sen!2snl!4v1557832838766!5m2!1sen!2snl" width="100%" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>            
+            </div>
+        </div>        
+    </div>
 </div>
