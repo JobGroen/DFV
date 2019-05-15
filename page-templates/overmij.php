b28efb7b9a1a3b3806c387ca2b18da92b2289cdd
IcemanHHW <22617@ma-web.nl>
2019-05-15 11:14:00 +0200

    Theme version 0.2.16
    Version 0.2.16 DFV Theme by:
    - Maarten kampmeijer
    
    This contains:
    - remade about me page
    - added responsive to about me page

--- page-templates/overmij.php
+++ page-templates/overmij.php
@@ -8,7 +8,7 @@
     </div>
 </div>
 
-<div class="diensten-single">
+<div class="aboutme-single">
     <div class="row">
         <div class="col-md-5">
             <div class="container">
@@ -17,20 +17,20 @@
 
         <div class="col-md-7">
             <div class="container">
-                <h2><?php the_field("overmij_titel")?></h2>
-                <div class="diensten-single-box">
-                    <img src="<?php the_field("overmij_afbeelding")?>" alt="overmij">
+                <div class="balk"></div>
+                <img class="aboutme-image-mobile" src="<?php the_field("overmij_afbeelding") ?>" alt="overmij">
+                <h2><?php the_field("overmij_titel") ?></h2>
+                <div class="aboutme-single-box">
+                    <img src="<?php the_field("overmij_afbeelding") ?>" alt="overmij">
                     <p><?php the_field("overmij_beschrijving") ?></p>
                 </div>
             </div>
         </div>
     </div>
 </div>
-<center>
-    <div class="balk"></div>
-    <iframe class="film" width="850" height="500" src="https://www.youtube.com/embed/tgbNymZ7vqY">
-    </iframe>
-</center>
+
+<div class="balk"></div>
+<iframe class="aboutme-video" src="https://www.youtube.com/embed/tgbNymZ7vqY"> </iframe>
 
 <?php get_template_part( 'template-parts/contactform'); ?>
 
