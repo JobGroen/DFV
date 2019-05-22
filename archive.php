76ff7c1fa0e1e47bfc451f2bbcf999e75f4a4e14
Job Groen <jobgroen98@gmail.com>
2019-05-22 10:24:59 +0200

    Theme version 0.3.4
    Version 0.3.4 DFV Theme by:
    - Job Groen
    
    This contains:
    - Landingpage blog
    - Responsive Landingspage blog
    - additional css fixed

--- archive.php
+++ archive.php
@@ -1,31 +1,34 @@
+<?php /* Template Name: Landings page blog */ ?>
+<?php $featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full'); ?>
 <?php get_header();?>
 
-<div class="container pt-5 pb-5">
-
-    <h1><?php single_cat_title();?></h1>
-
-    <?php if (have_posts()) : while(have_posts()) : the_post();?>
-
-    <div class="card">
-
-      <div class="card-body">
-
-          <?php if(has_post_thumbnail()):?>
-    
-              <img src="<?php the_post_thumbnail_url('smallest');?>" class="img-fluid">
-
-          <?php endif;?>
-
-        <h3><?php the_title();?></h3>
-        <?php the_excerpt();?>
-        <a href="<?php the_permalink();?>" class="btn btn-success">Lees meer</a>    
-
-      </div>
-
-    </div>
-
-    <?php endwhile; endif;?>
-
+<div id="header" style="background-image: url('/wp-content/uploads/2019/05/wenkbrouwen-wimpers.jpg')">
+  <div class="header">
+    <h1 class="standard-h1">Blog</h1>
+  </div>
 </div>
 
+<div class="container blog">
+  <?php if (have_posts()) : while(have_posts()) : the_post();?>
+
+  <div class="row pt-4 pb-4 no-gutters">
+    <div class="col-md-3 blog-leftside">
+      <?php if(has_post_thumbnail()):?>
+       <img src="<?php the_post_thumbnail_url('largest');?>" class="img-fluid">
+      <?php endif;?>
+    </div>
+    <div class="col-md-9 blog-rightside">
+      <div class="blog-righttext">
+      <h3><?php the_title();?></h3>
+        <?php the_excerpt();?><br>
+      <a href="<?php the_permalink();?>" class="standard-button">Lees meer</a>
+      </div>
+    </div>
+  </div>
+
+  <?php endwhile; endif;?>
+</div>
+
+
+<?php get_template_part( 'template-parts/contactform'); ?>
 <?php get_footer();?>
